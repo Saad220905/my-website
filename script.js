f19777d018022b0f199c784c862e86649a670883
Saad Kashif <saad220905@gmail.com>
2024-10-26 17:31:12 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript functionality can be added here for dynamic content or interactivity.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio website loaded.');
+});
